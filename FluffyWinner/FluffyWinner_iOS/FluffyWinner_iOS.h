32319e849543375d10583907103c012acc57743c
Ariel Rodriguez <ariel@volonbolon.me>
2020-04-27 16:44:30 -0300

    Add FluffyWinner_iOS framework

---
+++ FluffyWinner/FluffyWinner_iOS/FluffyWinner_iOS.h
@@ -0,0 +1,19 @@
+//
+//  FluffyWinner_iOS.h
+//  FluffyWinner_iOS
+//
+//  Created by Ariel Rodriguez on 27/04/2020.
+//  Copyright © 2020 Ariel Rodriguez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FluffyWinner_iOS.
+FOUNDATION_EXPORT double FluffyWinner_iOSVersionNumber;
+
+//! Project version string for FluffyWinner_iOS.
+FOUNDATION_EXPORT const unsigned char FluffyWinner_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FluffyWinner_iOS/PublicHeader.h>
+
+

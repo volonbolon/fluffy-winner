b1004846429a4d31d4b58eb95009994dc28b6aa5
Ariel Rodriguez <ariel@volonbolon.me>
2020-04-27 16:51:34 -0300

    Add FluffyWinnerUIKit

---
+++ FluffyWinner/FluffyWinnerUIKit/FluffyWinnerUIKit.h
@@ -0,0 +1,19 @@
+//
+//  FluffyWinnerUIKit.h
+//  FluffyWinnerUIKit
+//
+//  Created by Ariel Rodriguez on 27/04/2020.
+//  Copyright © 2020 Ariel Rodriguez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FluffyWinnerUIKit.
+FOUNDATION_EXPORT double FluffyWinnerUIKitVersionNumber;
+
+//! Project version string for FluffyWinnerUIKit.
+FOUNDATION_EXPORT const unsigned char FluffyWinnerUIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FluffyWinnerUIKit/PublicHeader.h>
+
+
---
+++ FluffyWinner/FluffyWinner_iOS/AppDependencyContainer.swift
@@ -0,0 +1,17 @@
+//
+//  AppDependencyContainer.swift
+//  FluffyWinner_iOS
+//
+//  Created by Ariel Rodriguez on 27/04/2020.
+//  Copyright © 2020 Ariel Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+public struct AppDependencyContainer {
+    public init() {}
+
+    public func makeMainViewController() -> UIViewController {
+
+    }
+}
